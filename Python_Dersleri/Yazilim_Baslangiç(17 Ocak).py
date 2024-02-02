8c0b267a538b73d46be29e2792fcb0dbba86540c
Osman <75824232+osman1357@users.noreply.github.com>
2024-02-02 18:32:49 +0300

    Add files via upload

---
+++ Python_Dersleri/Yazilim_Baslangiç(17 Ocak).py
@@ -0,0 +1,48 @@
+sayi1 = 42
+sayi2 = -17
+print(type(sayi1))
+
+
+sayi3 = 1.5
+sayi4 = -5.5
+print(type(sayi3))
+
+
+dogru_mu = True
+yanlis_mi = False
+print(type(dogru_mu))
+
+
+sayi1 = 42
+sayi2 = -17
+sayi3 = 1.5
+sayi4 = -5.5
+dogru_mu = True
+yanlis_mi = False
+sayi_float = float(sayi1)
+print("int to float", sayi_float)
+
+
+ondalik1 = 15
+sayi_int = int(ondalik1)
+print("float to int",sayi_int)
+
+
+dogru_mu = True
+sayi_bool = int(dogru_mu)
+print("bool to int", sayi_bool)
+
+
+kullanici_ad = input("Lütfen Adınızı Giriniz:")
+print("Hoşgeldin", kullanici_ad)
+
+
+sayi5 = int(input("Lütfen 1. Sayıyı Giriniz:"))          #inte girişte dönüştürüyor
+sayi6 = int(input("Lütfen 2. Sayıyı Giriniz:"))
+print(sayi5 + sayi6)
+
+sayi5 = input("Lütfen 1. Sayıyı Giriniz:")               #inte çıkışta dönüştürüyor
+sayi6 = input("Lütfen 2. Sayıyı Giriniz:")
+print(int(sayi5)+int(sayi6))
+
+
---
+++ Python_Dersleri/Yazilim_Baslangiç(20 Ocak)v2.py
@@ -0,0 +1,104 @@
+#list
+
+liste = [1,2,3,4,5]  
+print(liste)          # listeyi yazdırır  
+
+print(liste[4])       #listenin 5. elemanı
+
+print(type(liste))    #yaptığının ne tür olduğunu belirtiyor
+
+liste[2] = 100        
+print(liste)
+
+alt_liste = liste[2:4]
+print(alt_liste)
+#----------------------------------------------
+tuple
+
+tuple = (10,20,30,40,50)
+print(tuple)
+print(tuple[1])
+
+tuple[1] = 50
+print(tuple)
+
+alt_tuple = tuple[1:4]
+print(alt_tuple)
+#-----------------------------------------------
+#kümeler  (set-sets)
+
+kume = {100,200,300,400,500}
+print(kume)
+
+kume.add(700)
+print(kume)
+kume.update([400,900])
+print(kume)
+#-----------------------------------------------
+#dict dictionary sözlük
+
+dict ={'anahtar1':'deger1','anahtar2':'deger2'}
+print(dict)
+
+deger = dict['anahtar1']
+print(deger)
+
+kume_liste = list(kume)
+print(kume_liste)
+print(type(kume_liste))
+
+kume_liste = list(kume)
+print(kume_liste)
+print(type(kume_liste))
+
+
+
+list_keys = list(dict.keys())
+list_values = list(dict.values())             #aynısı
+print("keys",list_keys) 
+print("values",list_values)
+
+list_keys = list(dict.keys())               #aynısı
+print(type(list_keys))
+print(list_keys)
+list_values = list(dict.values())
+print(type(list_values))
+print(list_values)
+
+
+
+sayilar =[10,8,5,3,15,10]
+en_buyuk = max(sayilar)
+en_kucuk = min(sayilar)
+print(en_buyuk)
+print(en_kucuk)
+sayilar.append(20)
+sayilar.append(1)
+
+
+yeni_en_buyuk = max(sayilar)
+yeni_en_kucuk = min(sayilar)
+
+print(yeni_en_buyuk)
+print(yeni_en_kucuk)
+print(sayilar)
+
+sayilar.pop()                    #pop yapısı sona geleni siler                 #fifo ilk giren ilk çıkan , lifo son giren son çıkan
+print(sayilar)
+
+sayilar.sort()
+print(sayilar)                      #sayıları küçükten büyüğe sıralar           
+
+sayilar.reverse()                   #sayıları büyükten küçüğe sıralar
+print(sayilar)
+
+print(sayilar.count(10))            #sayıdan kaç tane olduğunu gösterir
+
+aralik = range(1,100)
+print(list(aralik))
+
+import random
+
+rastgele_sayi = random.randint(1,100)
+print("tutulan sayi",rastgele_sayi)
+
